539182d7f3380ae670bdf65ba67034178f3cfb8a
Jason Carney <jason.carney@towerswatson.com>
2020-12-02 12:33:41 -0500

    Formatted code

--- RPA.KnowledgebaseSync/RPA.KnowledgebaseSync.Activities/Utilities/Knowledgebase.cs
+++ RPA.KnowledgebaseSync/RPA.KnowledgebaseSync.Activities/Utilities/Knowledgebase.cs
@@ -18,12 +18,13 @@ namespace RPA.KnowledgebaseSync.Activities.Utilities
             QnADocumentsDTO qnaDocumentsDTO = (QnADocumentsDTO)JsonConvert.DeserializeObject<QnADocumentsDTO>(knowledgebase);
             new List<FileDTO>()
             {
-            new FileDTO()
-            {
-                FileName = (string) null,
-                FileUri = (string) null
-            }
+                new FileDTO()
+                {
+                    FileName = (string) null,
+                    FileUri = (string) null
+                }
             };
+
             List<MetadataDTO> metadataDtoList = new List<MetadataDTO>();
             KnowledgebaseUtility.LoadMetadata(qnaDocumentsDTO.QnaDTO, metadataDtoList);
             List<QnADTO> qnADTOs = new List<QnADTO>();
@@ -32,6 +33,7 @@ namespace RPA.KnowledgebaseSync.Activities.Utilities
             KnowledgebaseUtility.LoadUpdateQnADTOs(portalDbRecords, qnaDocumentsDTO, updateQnADTOs);
             IEnumerable<MetadataDTO> metadataDtos = metadataDtoList.Where<MetadataDTO>((Func<MetadataDTO, bool>)(m => m.Name == "faqid")).Where<MetadataDTO>((Func<MetadataDTO, bool>)(m => !portalDbRecords.Any<PortalDbRecordDTO>((Func<PortalDbRecordDTO, bool>)(p => p.FaqId.ToString() == m.Value))));
             List<int> intList = new List<int>();
+            
             foreach (MetadataDTO metadataDto in metadataDtos)
             {
                 foreach (QnADTO qnAdto in qnaDocumentsDTO.QnaDTO)
@@ -41,23 +43,27 @@ namespace RPA.KnowledgebaseSync.Activities.Utilities
                         intList.Add(qnAdto.Id);
                 }
             }
+
             Add add = new Add()
             {
                 Files = (List<FileDTO>)null,
                 QnaList = qnADTOs,
                 Urls = new List<string>()
             };
+
             Update update = new Update()
             {
                 Name = knowledgebaseName,
                 QnaList = updateQnADTOs,
                 Urls = new List<string>()
             };
+
             Delete delete = new Delete()
             {
                 Ids = intList,
                 Sources = new List<string>()
             };
+
             return JsonConvert.SerializeObject((object)new UpdateKbOperationDTO()
             {
                 Add = add,
@@ -79,15 +85,19 @@ namespace RPA.KnowledgebaseSync.Activities.Utilities
                 questions.Add = new List<string>();
                 Metadata metadata = new Metadata();
                 metadata.Add = new List<MetadataDTO>();
+                
                 foreach (QnADTO qnAdto in qnaDocumentsDTO.QnaDTO)
                 {
                     IEnumerable<string> source = qnAdto.Metadata.Where<MetadataDTO>((Func<MetadataDTO, bool>)(m => m.Name.ToLower() == "faqid")).Select<MetadataDTO, string>((Func<MetadataDTO, string>)(m => m.Value));
+                    
                     if (source.FirstOrDefault<string>() != null)
                     {
                         string str = source.FirstOrDefault<string>().ToString();
+                        
                         if (portalDbRecord.FaqId.ToString() == str)
                         {
                             questions.Add.Add(portalDbRecord.FaqQuestion);
+                            
                             foreach (MetadataDTO metadataDto1 in qnAdto.Metadata)
                             {
                                 MetadataDTO metadataDto2 = new MetadataDTO()
@@ -97,6 +107,7 @@ namespace RPA.KnowledgebaseSync.Activities.Utilities
                                 };
                                 metadata.Add.Add(metadataDto2);
                             }
+                            
                             UpdateQnaDTO updateQnaDto = new UpdateQnaDTO()
                             {
                                 Answer = portalDbRecord.FaqAnswer,
@@ -106,6 +117,7 @@ namespace RPA.KnowledgebaseSync.Activities.Utilities
                                 Questions = questions,
                                 Source = qnAdto.Source
                             };
+                            
                             updateQnADTOs.Add(updateQnaDto);
                         }
                     }
@@ -123,6 +135,7 @@ namespace RPA.KnowledgebaseSync.Activities.Utilities
                 bool flag = false;
                 string str = "";
                 List<string> source = new List<string>();
+                
                 foreach (QnADTO qnAdto in qnaDocumentsDTO.QnaDTO)
                 {
                     foreach (MetadataDTO metadataDto in qnAdto.Metadata)
@@ -130,9 +143,11 @@ namespace RPA.KnowledgebaseSync.Activities.Utilities
                         if (metadataDto.Name == "faqid")
                             str = metadataDto.Value;
                     }
+                    
                     if (str == portalDbRecord.FaqId.ToString())
                         flag = true;
                 }
+                
                 if (!flag)
                 {
                     List<MetadataDTO> metadataDtoList = new List<MetadataDTO>();
@@ -141,31 +156,43 @@ namespace RPA.KnowledgebaseSync.Activities.Utilities
                         Name = "faqcategoryid",
                         Value = portalDbRecord.FaqCategoryFaqId.ToString()
                     });
+                    
                     MetadataDTO metadataDto1 = new MetadataDTO();
                     metadataDto1.Name = "sortOrder";
+                    
                     int num = portalDbRecord.FaqSortOrder;
                     metadataDto1.Value = num.ToString();
+
                     MetadataDTO metadataDto2 = metadataDto1;
                     metadataDtoList.Add(metadataDto2);
+
                     MetadataDTO metadataDto3 = new MetadataDTO();
                     metadataDto3.Name = "faqid";
+
                     num = portalDbRecord.FaqId;
                     metadataDto3.Value = num.ToString();
+
                     MetadataDTO metadataDto4 = metadataDto3;
                     metadataDtoList.Add(metadataDto4);
+
                     MetadataDTO metadataDto5 = new MetadataDTO();
                     metadataDto5.Name = "portalid";
+
                     num = portalDbRecord.PortalId;
                     metadataDto5.Value = num.ToString();
+
                     MetadataDTO metadataDto6 = metadataDto5;
                     metadataDtoList.Add(metadataDto6);
+                    
                     MetadataDTO metadataDto7 = new MetadataDTO()
                     {
                         Name = "iscommonlyasked",
                         Value = portalDbRecord.IsCommonlyAsked.ToString()
                     };
+                    
                     metadataDtoList.Add(metadataDto7);
                     source.Add(portalDbRecord.FaqQuestion);
+                    
                     QnADTO qnAdto = new QnADTO()
                     {
                         Answer = portalDbRecord.FaqAnswer,
@@ -173,6 +200,7 @@ namespace RPA.KnowledgebaseSync.Activities.Utilities
                         Questions = source.ToList<string>(),
                         Source = "FAQ"
                     };
+                    
                     qnADTOs.Add(qnAdto);
                 }
             }
@@ -183,6 +211,7 @@ namespace RPA.KnowledgebaseSync.Activities.Utilities
             foreach (QnADTO qnAdto in qnADTOs.ToArray())
             {
                 MetadataDTO[] array = qnAdto.Metadata.ToArray();
+                
                 for (int index = 0; index < array.Length; ++index)
                 {
                     MetadataDTO metadataDto = new MetadataDTO()
@@ -190,6 +219,7 @@ namespace RPA.KnowledgebaseSync.Activities.Utilities
                         Name = array[index].Name,
                         Value = array[index].Value
                     };
+                    
                     metadataDTOs.Add(metadataDto);
                 }
             }
